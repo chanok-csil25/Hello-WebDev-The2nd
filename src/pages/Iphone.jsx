2f3a2d08f2e7b254aa95d28754caf7ec4ce5f3d3
Chitchanok Sillapa <142306071+chanok-csil25@users.noreply.github.com>
2023-12-09 11:22:16 +0700

    Update Iphone.jsx

--- src/pages/Iphone.jsx
+++ src/pages/Iphone.jsx
@@ -1,5 +1,5 @@
 import ProductDetail from '../components/ProductDetail'
-import iphone from '../assets/product/Iphone.png'
+import iphone from '../assets/product/iphone.png'
 function Iphone() {
     return (
         <ProductDetail img={iphone} productname={" 13 MODIE "} price={"PRICE = 20,400 THB"} description={["-สินค้ารับประกัน 1 ปี ", "-จอภาพ Super Retina XDR ขนาด 6.1 นิ้ว2", "-กล้องหน้า TrueDepth ความละเอียด 12MP", "-ชิป A15 Bionic พร้อม GPU แบบ 4-core ระบบเซลลูลาร์ 5G ที่เร็วสุดแรง1"]} />
